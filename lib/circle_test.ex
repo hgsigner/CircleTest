b9672059806f33d59146ba6698a4fbcd838fdfef
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-07-27 23:49:58 -0700

    find the safer place

--- lib/circle_test.ex
+++ lib/circle_test.ex
@@ -1,2 +1,35 @@
 defmodule CircleTest do
-end
+
+	def remaining_place(arr, index) do
+		process_list(arr, index) |> find_place(0)
+	end
+
+	defp process_list(arr, index) when index == 0, do: arr
+
+	defp process_list(arr, index) do
+		Enum.slice(arr, index, length(arr)) ++ Enum.slice(arr, 0, index)
+	end
+
+	defp find_place(arr, start_at) when length(arr) > 1 do
+		current_last_item = List.last(arr)
+
+		ordered_array = Enum.with_index(arr)
+		|> Enum.filter(fn({_,i}) -> 
+		 	if start_at == 0, do: rem(i, 2) == 0, else: rem(i, 2) != 0
+		end)
+		|> Enum.map(fn({v,_}) -> v end)
+
+		last_item = List.last(ordered_array)
+
+		cond do
+			current_last_item == last_item ->
+				find_place(ordered_array, 1)
+			current_last_item != last_item ->
+				find_place(ordered_array, 0)
+		end
+		
+	end
+
+	defp find_place(arr, _) when length(arr) == 1, do: List.last(arr)
+
+end    
--- test/circle_test_test.exs
+++ test/circle_test_test.exs
@@ -1,7 +1,9 @@
 defmodule CircleTestTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "has the right remainng place" do
+    assert CircleTest.remaining_place([1,2,3,4,5,6,7,8,9,10], 0) == 5
+    assert CircleTest.remaining_place([1,2,3,4], 0) == 1
+    assert CircleTest.remaining_place([1,2,3,4], 1) == 2
   end
 end
